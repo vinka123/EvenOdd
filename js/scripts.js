a3b52492095f51cce1560e98f74ce4618a1d41fe
vinka123 <ktnvctr123@gmail.com>
2020-04-09 20:18:10 +0300

    Add js code

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+alert('This is a simple program that determines whether a number is Even or Odd');
+var numb=parseInt(prompt('Enter any number from 1-10'));
+if(numb==2) {
+alert('Even number');
+} else if(numb==4) {
+alert('Even number');
+} else if(numb==6) {
+alert('Even number');
+} else if(numb==8) {
+alert('Even number');
+} else if(numb==10) {
+alert('Even number');
+} else {
+alert('Odd number');
+}
